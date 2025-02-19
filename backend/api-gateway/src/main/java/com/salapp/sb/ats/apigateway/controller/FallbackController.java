9499f9bc2336e44624a93805691213e7dac20f57
Stainley Lebron <stainley.lebron@gmail.com>
2025-02-19 14:53:15 -0500

    flutter module, angular module, react vite module

--- backend/api-gateway/src/main/java/com/salapp/sb/ats/apigateway/controller/FallbackController.java
+++ backend/api-gateway/src/main/java/com/salapp/sb/ats/apigateway/controller/FallbackController.java
@@ -1,4 +1,15 @@
 package com.salapp.sb.ats.apigateway.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
 public class FallbackController {
+
+    @GetMapping("/user")
+    public String userFallback() {
+        return "User Service is currently unavailable. Please try again later.";
+    }
 }
