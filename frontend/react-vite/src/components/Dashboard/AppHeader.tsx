6189661ac0d2c55af33f075e97fedd6994f4516b
Stainley <stainley.lebron@gmail.com>
2025-03-10 23:54:10 -0400

    info executed

--- frontend/react-vite/src/components/Dashboard/AppHeader.tsx
+++ frontend/react-vite/src/components/Dashboard/AppHeader.tsx
@@ -2,6 +2,7 @@ import { AppBar, Toolbar, IconButton, Typography, Avatar, Tooltip, Box } from '@
 import { Menu as MenuIcon } from '@mui/icons-material';
 import { UserData } from '../../types';
 import { DRAWER_WIDTH } from '../../constants';
+
 import React from 'react';
 
 import profilePic from '../../assets/user.png';
